eb37d28d0acd53dfe8bf3a2015bb16409336b10f
Chris Dallat <chrisdallat@Chriss-MacBook-Pro.local>
2021-10-26 18:23:05 +0100

    need to convert to RPN

--- src/main.c
+++ src/main.c
@@ -1,20 +1,26 @@
 #include "../include/my_bc.h"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 
-    if(argc < 2){
+    if (argc < 2)
+    {
         print_error("usage");
         return 1;
     }
 
-    if(count_open_closed(argv[1]) != 0){
+    if (count_open_closed(argv[1]) != 0)
+    {
         print_error("parentheses");
         return 1;
     }
+    printf("\ninput string: %s\n\n", argv[1]);
+    //function to convert to Reverse Polish Notation needed here to be able to evaluate
+    //e.g "5+65*7" -> "5 65 * 7 +" (probably add whitespaces first in order to seperate the numbers)
 
-    token *new_lexer = malloc(sizeof(token)*1);
+    token *new_lexer = malloc(sizeof(token) * 1);
 
-    if((new_lexer = get_lexer(argv[1])))
+    if ((new_lexer = get_lexer(argv[1])))
         printf("Success\n");
     else
         printf("Error\n");
