4029d995a9aa22d2662f2589d17027ecf4315946
khalilmasri <khalil.1996km@gmail.com>
2021-10-27 18:52:46 +0300

    Lexer implementation

--- src/lexer.c
+++ src/lexer.c
@@ -8,97 +8,103 @@ int check_consuctive_number(char *str, int index){
         return 0; 
 }
 
-int count_tokens(char *argv){
+int count_tokens(char *formula){
 
     int counter = 0,
         index = 0;
 
-    while(argv[index]){
-        
-        if(argv[index] != ' ')
+    while(formula[index]){
+
+        if(formula[index] == ' ')
             counter++;
-        else if(check_consuctive_number(argv, index) == 1)
-            return -1;
 
         index++;
     }
 
-    return counter;
+    return counter+1;
 }
 
-token *get_token_attribute(char c, token *token, int index){
-
-    switch(c){
-        case '+':
-            my_strcpy(token->token_type[index],ADD);
-            token->priority[index] = PRIORITY_THREE;
-            break;
-        case '-':
-            my_strcpy(token->token_type[index],SUB);
-            token->priority[index] = PRIORITY_THREE;
-            break;
-        case '*':
-            my_strcpy(token->token_type[index],MULT);
-            token->priority[index] = PRIORITY_TWO;
-            break;
-        case '/':
-            my_strcpy(token->token_type[index],DIV);
-            token->priority[index] = PRIORITY_TWO;
-            break;
-        case '%':
-            my_strcpy(token->token_type[index],SUB);
-            token->priority[index] = PRIORITY_TWO;
-            break;
-        case '(':
-            my_strcpy(token->token_type[index], OPEN_PAR);
-            token->priority[index] = PRIORITY_FOUR;
-            break;
-        case ')':
-            my_strcpy(token->token_type[index], CLOSE_PAR);
-            token->priority[index] = PRIORITY_ONE;
-            break;
-        default:
-            if(is_digit(c) == 0){
-                my_strcpy(token->token_type[index],VAL);
-                token->priority[index] = PRIORITY_FIVE;
-            }else 
-                return NULL;
-            break;
-    }
-
-    return token;
-}
-
-token *get_tokens(char *argv, token *token){
+token *get_token_attribute(token *token){
 
     int index = 0,
-        cur = 0;
+        current = 0;
 
-    while(argv[index]){
-        if(argv[index] != ' '){
-            token->tokens[cur][0] = argv[index];
-            token = get_token_attribute(argv[index], token, index);
-
-            if(token == NULL)
-                return NULL;
-
-            printf("token: %c type: %s Priority: %d\n", token->tokens[cur][0], token->token_type[cur], token->priority[cur]);
-            cur++;
-        }
-
+    while(current < token->token_count){
+        switch(token->tokens[current][0]){
+            case '+':
+                my_strcpy(token->token_type[index], ADD);
+                token->priority[index] = PRIORITY_THREE;
+                break;
+            case '-':
+                my_strcpy(token->token_type[index],SUB);
+                token->priority[index] = PRIORITY_THREE;
+                break;
+            case '*':
+                my_strcpy(token->token_type[index],MULT);
+                token->priority[index] = PRIORITY_TWO;
+                break;
+            case '/':
+                my_strcpy(token->token_type[index],DIV);
+                token->priority[index] = PRIORITY_TWO;
+                break;
+            case '%':
+                my_strcpy(token->token_type[index],MOD);
+                token->priority[index] = PRIORITY_TWO;
+                break;
+            case '(':
+                my_strcpy(token->token_type[index], OPEN_PAR);
+                token->priority[index] = PRIORITY_FOUR;
+                break;
+            case ')':
+                my_strcpy(token->token_type[index], CLOSE_PAR);
+                token->priority[index] = PRIORITY_ONE;
+                break; 
+            default:
+                if(is_digit(token->tokens[current][0]) == 1){
+                    my_strcpy(token->token_type[index], VAL);
+                    token->priority[index] = PRIORITY_FIVE;
+                }else{
+                    return NULL;
+                }
+        } 
         index++;
+        current++;
+    }
+    return token;
+}
+
+token *get_tokens(char *formula, token *token){
+
+    int index = 0,
+        cur = 0,
+        i = 0;
+
+    char* new_token = malloc(sizeof(char)*MAX_STR_LEN);
+
+    while(formula[index] != '\0'){
+        while(formula[index] != ' ' && formula[index] != '\0'){
+            new_token[i] = formula[index];
+            index++;
+            i++;
+        }
+        new_token[i] = '\0';
+        my_strcpy(token->tokens[cur], new_token);
+
+        cur++;
+        index++;
+        i = 0;
     }
 
     return token;
 }
 
-token *get_lexer(char *argv){
+token *get_lexer(char *formula){
 
     token *new_token = malloc(sizeof(token)*1);
 
-    if((new_token->token_count = count_tokens(argv)) == -1)
-         return NULL;
-    
+    if((new_token->token_count = count_tokens(formula)) == -1)
+        return NULL;
+
     new_token->tokens = malloc(sizeof(char*)*new_token->token_count);
     new_token->token_type = malloc(sizeof(char*)*new_token->token_count);
     new_token->priority = malloc(sizeof(int)*new_token->token_count);
@@ -108,7 +114,8 @@ token *get_lexer(char *argv){
         new_token->tokens[i] = malloc(sizeof(char)*MAX_TYPE_LEN);
     }
 
-    new_token = get_tokens(argv, new_token);
+    new_token = get_tokens(formula, new_token);
+    new_token = get_token_attribute(new_token);
 
     if(!new_token)
         return NULL;
